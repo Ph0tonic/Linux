5f538f43bff03d848f6d000728fe1ebb018e482a
bull0n <lucas.bulloni@he-arc.ch>
2017-05-24 10:57:31 +0200

    changement indentation + problème MIM

--- web7.5.py
+++ web7.5.py
@@ -7,14 +7,14 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 # HTTPRequestHandler class
 class snmpHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
-  # GET
-  def do_GET(self):
+    # GET
+    def do_GET(self):
         # Send response status code
         self.send_response(200)
-
+        
         # Send headers
         #changer en SVG
-        self.send_header('Content-type','image/text')
+        self.send_header('Content-type','text/html')
         self.end_headers()
 
         # Send message back to client
@@ -24,14 +24,14 @@ class snmpHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         return
 
 def run():
-  print('starting server...')
+    print('starting server...')
 
-  # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-  server_address = ('127.0.0.1', 80)
-  httpd = HTTPServer(server_address, snmpHTTPServer_RequestHandler)
-  print('running server...')
-  httpd.serve_forever()
+    # Server settings
+    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+    server_address = ('127.0.0.1', 80)
+    httpd = HTTPServer(server_address, snmpHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
 
 
 run()
