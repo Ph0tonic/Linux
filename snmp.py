6f671ce47b4b1a8c872782ad63627953cee77a0b
Malik Fleury <malik.fleury@he-arc.ch>
2017-06-02 15:22:06 +0200

    SNMP

--- snmp.py
+++ snmp.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python3
 
+from threading import Timer
+from time import time
 from pysnmp.hlapi import *
 from pysnmp.smi.view import MibViewController
 
 
-#SOURCES : https://makina-corpus.com/blog/metier/2016/initiation-a-snmp-avec-python-pysnmp-partie2
-#        : http://pysnmp.sourceforge.net/docs/hlapi/asyncore/sync/manager/cmdgen/getcmd.html
-def snmp_read_counter_bytes():
-    ip = '157.26.77.13'     # Ip de l'agent
-    port = 161              # Port
+class SnmpTool:
 
-    se = SnmpEngine()                        # Coeur de la bibliothèque pySNMP
-    cd = CommunityData('public', mpModel=0)  # Paramètres d'auth : SNMPv1
-    utt = UdpTransportTarget((ip, port))     # Protocole C4 : UDP
-    ot1 = ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.10.1'))  # Identifiant info. : nombre d'octets sur interface 1
-    ot2 = ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.10.2'))  # Identifiant info. : nombre d'octets sur interface 2
+    def __init__(self,ip,port, log_filename):
+        self.ip = ip
+        self.port = port
+        self.log_filename = log_filename
+        self.engine = SnmpEngine()                              # Coeur de pySNMP
+        self.community = CommunityData('public', mpModel=0)     # Communauté
+        self.transport = UdpTransportTarget((ip,port))          # Protocole couche 4 : UDP
+        self.logfile = open(log_filename, 'a')
 
-    # Generation de la commande et envoi
-    errIndic, errStat, errIndexgcmd, varBinds = next(getCmd(se, cd, utt, ContextData(), ot1, ot2))
+    def read(self, object_type_id):
+        # Generation de la commande et envoi
+        err_indic, err_stat, err_index, var_binds = next(getCmd(self.engine, self.community,
+                                                                self.transport, ContextData(),
+                                                                object_type_id))
 
-    # Gestion des erreurs et affichage du résultat si OK
-    if errIndic:
-        print(errIndic)
-    else:
-        if errStat:
-            print('bug')
+        # Gestion des erreurs et affichage du résultat si OK
+        if err_indic:
+            print(err_indic)
         else:
-            # ajout des données dans le fichier
-            for name, val in varBinds:
-                print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+            if err_stat:
+                print('bug')
+            else:
+                # ajout des données dans le fichier
+                for name, val in var_binds:
+                    print ("test")
+                    self.logfile.write(str(int(time())) + ' %s\n' % ( val.prettyPrint()))
 
-snmp_read_counter_bytes()
+
+snmpt = SnmpTool('157.26.77.13', 161, "historique")
+ot = ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.10.1'))
+snmpt.read(ot)
