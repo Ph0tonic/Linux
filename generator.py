82f2e01f0a400aa7b308369d9a2c824a72eb9f57
Ph0tonic <bastien@debian>
2017-06-02 14:57:19 +0200

    - Commentaire et nettoyage du code - Création d'une cartouche

--- generator.py
+++ generator.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
 
+# Description : Programme permettant de générer un fichier SVG
+#               contenant un graphique à partir d'un fichier historique
+#               stockant les données du serveur snmp
+# Auteurs : Lucas Bulloni, Malik Fleury et Bastien Wermeille
+# Data : 02.06.2017
+# Version : 1.0
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# Lecture des données
 def ReadFile(filePath):
     time = []
     octets = []
     #Récupération des données
     with open(filePath, "r") as f:
         for line in f.readlines():
-            t, o = line.split(" ")
-            time.append(t)
-            octets.append(o)
+            if line != "\n" :
+                t, o = line.split(" ")
+                time.append(t)
+                octets.append(o)
         f.close()
         return (time,octets)
     return (None,None)
 
+# Generation du graphique
+def GenerateFile(data):
+    plt.plot(data[0], data[1])
+    plt.ylabel('bytes (o)')
+    plt.xlabel('time (s)')
+    plt.title('Bytes at time')
+    plt.grid(True)
+    plt.savefig("graph.svg",type="svg")
+    #plt.show()
 
-(time,octets) = ReadFile("historique")
-#print(type(data))
-#print(data)
-print(time)
-print(octets)
-
-plt.plot(time, octets)
-
-plt.xlabel('bytes (o)')
-plt.ylabel('time (s)')
-plt.title('Bytes at time')
-plt.grid(True)
-plt.savefig("graph.svg",type="svg")
-plt.show()
+# Main programme
+GenerateFile(ReadFile("historique"))
