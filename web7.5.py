5febcb98d727512e5086863bd4287b7c1f1cb105
bull0n <lucas.bulloni@he-arc.ch>
2017-06-02 15:00:19 +0200

    ajout de commentaire

--- web7.5.py
+++ web7.5.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+#!/usr/bin/env python3
+
+# Description : Serveur web qui permet l'affichage du graphique
+# Auteurs : Lucas Bulloni, Malik Fleury et Bastien Wermeille
+# Data : 02.06.2017
+# Version : 1.0
+
 #SOURCE CODE FROM https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -7,24 +14,21 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 # HTTPRequestHandler class
 class snmpHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
-    # GET
+    # GET , réécriture de la fonction de l'objet BaseHTTPRequestHandler
     def do_GET(self):
         # Send response status code
         self.send_response(200)
-
-        print (self.path)
         if self.path == "/graph.svg":
-            #SVG binaire
-            #self.send_header('Content-type','image/svg')
-            #svgFile = open('graph.svg', 'rb')
-            #svg xml
+            #svg xml envoie du contenu du fichier
             self.send_header('Content-type','image/svg+xml')
             svgFile = open('graph.svg', 'r')
             self.end_headers()
             self.wfile.write(bytes(svgFile.read(), 'utf8'))
             svgFile.close()
         else :
+            #envoie du text si c'est une autre page que l'image qui est demandé
             self.send_header('Content-type','text/html')
+            #code HTML
             message = '''<!DOCTYPE html>
                 <html>
                 <head>
@@ -44,7 +48,7 @@ class snmpHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
         return
 
-
+#fonction du démarrage du server web - REQUIERT DROIT SUDO CAR PORT 80
 def run():
     print('starting server...')
 
@@ -53,5 +57,5 @@ def run():
     print('running server...')
     httpd.serve_forever()
 
-
+#démarrage du server
 run()
